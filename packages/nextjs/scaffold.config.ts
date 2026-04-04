703fe6bbc3f57def9171214050ed744eb4cb09b6
binnace <abhishinde0143@gmail.com>
2026-04-05 02:43:50 +0530

    Update scaffold.config.ts

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -1,48 +1,24 @@
-import * as chains from "viem/chains";
+import { chains } from "@@/utils/scaffold-eth";
 
 export type ScaffoldConfig = {
   targetNetworks: readonly chains.Chain[];
   pollingInterval: number;
   alchemyApiKey: string;
-  rpcOverrides?: Record<number, string>;
   walletConnectProjectId: string;
-  burnerWalletMode: "localNetworksOnly" | "allNetworks" | "disabled";
+  onlyLocalBurnerWallet: boolean;
 };
 
-export const DEFAULT_ALCHEMY_API_KEY = "cR4WnXePioePZ5fFrnSiR";
-
 const scaffoldConfig = {
-  // The networks on which your DApp is live
-  targetNetworks: [chains.hardhat],
+  // Yahan humne Arbitrum set kar diya hai
+  targetNetworks: [chains.arbitrum],
 
-  // The interval at which your front-end polls the RPC servers for new data
-  // it has no effect if you only target the local network (default is 4000)
-  pollingInterval: 3000,
+  pollingInterval: 30000,
 
-  // This is ours Alchemy's default API key.
-  // You can get your own at https://dashboard.alchemyapi.io
-  // It's recommended to store it in an env variable:
-  // .env.local for local testing, and in the Vercel/system env config for live apps.
-  alchemyApiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || DEFAULT_ALCHEMY_API_KEY,
+  // Ye default keys hain, baad mein badal sakte ho
+  alchemyApiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "oKsh3787S7u_6uX7AofXf77K_Sjw7mXf",
+  walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "3a8170812b53d739d9f453005a610537",
 
-  // If you want to use a different RPC for a specific network, you can add it here.
-  // The key is the chain ID, and the value is the HTTP RPC URL
-  rpcOverrides: {
-    // Example:
-    // [chains.mainnet.id]: "https://mainnet.rpc.buidlguidl.com",
-  },
-
-  // This is ours WalletConnect's default project ID.
-  // You can get your own at https://cloud.walletconnect.com
-  // It's recommended to store it in an env variable:
-  // .env.local for local testing, and in the Vercel/system env config for live apps.
-  walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "3a8170812b534d0ff9d794f19a901d64",
-
-  // Configure Burner Wallet visibility:
-  // - "localOnly": only show when all target networks are local (hardhat/anvil)
-  // - "allNetworks": show on any configured target networks
-  // - "disabled": completely disable
-  burnerWalletMode: "localNetworksOnly",
+  onlyLocalBurnerWallet: false,
 } as const satisfies ScaffoldConfig;
 
 export default scaffoldConfig;
