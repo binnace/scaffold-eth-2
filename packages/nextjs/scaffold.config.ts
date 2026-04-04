b6cea2e3c79285a466e7c14650db9af33c8ecd93
binnace <abhishinde0143@gmail.com>
2026-04-05 03:34:06 +0530

    Update scaffold.config.ts

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -1,19 +1,9 @@
+import * as chains from "viem/chains";
 export const scaffoldConfig = {
-  targetNetworks: [
-    {
-      id: 42161,
-      name: "arbitrum",
-      nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
-      rpcUrls: {
-        default: { http: ["https://arb1.arbitrum.io/rpc"] },
-        public: { http: ["https://arb1.arbitrum.io/rpc"] },
-      },
-    },
-  ],
+  targetNetworks: [chains.arbitrum],
   pollingInterval: 30000,
   alchemyApiKey: "oKsh3787S7u_6uX7AofXf77K_Sjw7mXf",
   walletConnectProjectId: "3a8170812b53d739d9f453005a610537",
   onlyLocalBurnerWallet: false,
 };
-
 export default scaffoldConfig;
