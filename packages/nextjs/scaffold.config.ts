a840c537613f269bfb611cf4fefc202350d0b22f
binnace <abhishinde0143@gmail.com>
2026-04-05 03:24:36 +0530

    Update scaffold.config.ts

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -1,31 +1,19 @@
-export type ScaffoldConfig = {
-  targetNetworks: readonly any[];
-  pollingInterval: number;
-  alchemyApiKey: string;
-  walletConnectProjectId: string;
-  onlyLocalBurnerWallet: boolean;
-};
-
-const scaffoldConfig = {
+export const scaffoldConfig = {
   targetNetworks: [
     {
       id: 42161,
-      name: "Arbitrum One",
-      network: "arbitrum",
+      name: "arbitrum",
       nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
       rpcUrls: {
         default: { http: ["https://arb1.arbitrum.io/rpc"] },
         public: { http: ["https://arb1.arbitrum.io/rpc"] },
       },
-      blockExplorers: {
-        default: { name: "Arbiscan", url: "https://arbiscan.io" },
-      },
-    }
+    },
   ],
   pollingInterval: 30000,
-  alchemyApiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "oKsh3787S7u_6uX7AofXf77K_Sjw7mXf",
-  walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "3a8170812b53d739d9f453005a610537",
+  alchemyApiKey: "oKsh3787S7u_6uX7AofXf77K_Sjw7mXf",
+  walletConnectProjectId: "3a8170812b53d739d9f453005a610537",
   onlyLocalBurnerWallet: false,
-} as const satisfies ScaffoldConfig;
+};
 
 export default scaffoldConfig;
