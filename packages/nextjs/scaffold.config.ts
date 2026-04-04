490f38437cb99fa7ced06b2183f8ab2e1b733ccc
binnace <abhishinde0143@gmail.com>
2026-04-05 02:50:00 +0530

    Update scaffold.config.ts

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -10,12 +10,9 @@ export type ScaffoldConfig = {
 
 const scaffoldConfig = {
   targetNetworks: [chains.arbitrum],
-
   pollingInterval: 30000,
-
   alchemyApiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "oKsh3787S7u_6uX7AofXf77K_Sjw7mXf",
   walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "3a8170812b53d739d9f453005a610537",
-
   onlyLocalBurnerWallet: false,
 } as const satisfies ScaffoldConfig;
 
